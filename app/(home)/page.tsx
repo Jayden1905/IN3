a7d290ccf44624898628a669ded46a7e6c4de83c
Kyaw Za Yan Naing <93457245+Jayden1905@users.noreply.github.com>
2024-03-12 18:47:59 +0800

    update moudles

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,10 @@
-import WaveSection from "@/app/(home)/sections/waveSection";
-import HeroSection from "./sections/heroSection";
-import AboutSection from "./sections/aboutSection";
-import FeaturedCourses from "./sections/featuredCourses";
-import PotentialSection from "./sections/potentialSection";
+import WaveSection from '@/app/(home)/sections/waveSection'
+import HeroSection from './sections/heroSection'
+import AboutSection from './sections/aboutSection'
+import FeaturedCourses from './sections/featuredCourses'
+import PotentialSection from './sections/potentialSection'
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   return (
@@ -15,5 +15,5 @@ export default async function Home() {
       <AboutSection />
       <PotentialSection />
     </>
-  );
+  )
 }
