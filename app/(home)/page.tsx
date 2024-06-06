de9aee57b37f2d991de926caac41e5fef97dd501
Kyaw Za Yan Naing <93457245+Jayden1905@users.noreply.github.com>
2024-06-06 15:59:33 +0800

    use switch case instead

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,5 +4,12 @@ export default async function Page() {
   const res = await fetch('https://ipinfo.io/json')
   const data = await res.json()
 
-  data.country === 'US' ? redirect('/us') : redirect('/sg')
+  switch (data.country) {
+    case 'SG':
+      redirect('/sg')
+    case 'US':
+      redirect('/us')
+    default:
+      redirect('/sg')
+  }
 }
