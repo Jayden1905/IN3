525ddc853be8a612c2c0d6d9092a07d2b6ce1188
Kyaw Za Yan Naing <93457245+Jayden1905@users.noreply.github.com>
2024-06-06 16:03:19 +0800

    change things

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   keywords: ['robotics', 'coding', 'education', 'kids'],
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
