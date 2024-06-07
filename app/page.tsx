da0e906eb8ff23ecd12d310ed3e7fe992cc3f9f9
Kyaw Za Yan Naing <93457245+Jayden1905@users.noreply.github.com>
2024-06-07 18:27:06 +0800

    change fetching style

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
-  const request = await fetch('https://ipinfo.io/json?token=996bb0edca1ff4')
-  const data = await request.json()
-
-  console.log(data)
-
-  switch (data.country) {
-    case 'SG':
-      redirect('/sg')
-    case 'US':
-      redirect('/us')
-    default:
-      redirect('/sg')
-  }
+  await fetch('https://ipinfo.io/json?token=996bb0edca1ff4').then((res) =>
+    res.json().then((data) => {
+      console.log(data)
+      switch (data.country) {
+        case 'SG':
+          redirect('/sg')
+        case 'US':
+          redirect('/us')
+        default:
+          redirect('/sg')
+      }
+    })
+  )
 }
