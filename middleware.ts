63b126fcbeb37cc5d0edef431ec417a0c854e579
Kyaw Za Yan Naing <93457245+Jayden1905@users.noreply.github.com>
2024-06-08 02:35:11 +0800

    next image error fix

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static).*)'],
 }
 
 // Fetch country code from API
